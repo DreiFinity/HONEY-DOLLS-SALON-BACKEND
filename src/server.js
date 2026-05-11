2dbce558f6e8bee1fea00371d4ec93c3ff01e480
DreiFinity <vladimaizumi@gmail.com>
2026-05-11 21:02:24 +0800

    fixed cors

--- src/server.js
+++ src/server.js
@@ -74,7 +74,13 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: ["https://honey-dolls-salon-frontend-xiz6.vercel.app", "http://localhost:5173"],
+  methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+  credentials: true,
+  allowedHeaders: ["Content-Type", "Authorization"]
+}));
+
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
